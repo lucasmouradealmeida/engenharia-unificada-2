e4da31331a21e014904dfb61df534e229758b5ac
Lucas Moura de Almeida <lucas.almeida@ape11.com.br>
2023-11-26 16:50:12 -0300

    Finalização da programação do sistema de comporta

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,59 @@
 // Variáveis Globais
 
 // Sensor ultrassonico 1
-
 const int triggerPin = 10;
 const int echoPin = 11;
-
-// Sensor ultrassonico 2
-
-const int triggerPin2 = 12;
-const int echoPin2 = 13;
 
 // Servo motor
 Servo myservo;
 const int servoPin = 3;
 
+// Valor de calibração
+float calibracao = 60.0;
+int numLeiturasCalibracao = 100;
 
 // Declaração de funções
-int ultrasomDistance();
-int moveServo();
+float ultrasomDistance();
+void moveServo(int angle);
+void calibrarSensor();
 
 void setup() {
-
   Serial.begin(115200);
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  pinMode(triggerPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-
   myservo.attach(servoPin);
+  myservo.write(0);
 
+  calibrarSensor();
 }
 
 void loop() {
+  float distance = ultrasomDistance();
 
-  int distance = ultrasomDistance();
-  Serial.print("Distância em cm: ");
-  Serial.print(distance);
-
-  if(distance < 10){
-    int move = moveServo();
-    Serial.print("Movendo servo: ");
-    Serial.print(move);
+  // Condição de giro para o servo
+  if (distance > 9.99) {
+    moveServo(0);
+    Serial.println("Tanque vazio -> Distância: " + String(distance) + "cm");
+  } else if (distance < 9.99 && distance > 4.99) {
+    Serial.println("Tanque em níveis seguros -> Distância: " + String(distance) + "cm");
+  } else if (distance < 4.99) {
+    moveServo(90);
+    Serial.println("Estabilizando para níveis seguros -> Distância: " + String(distance) + "cm");
   }
-
 }
 
 // Função para obter as distâncias dos sensores ultrassonicos
-int ultrasomDistance(){
+#define NUM_READINGS 15
 
-    long duration;
-    float distance_cm;
+float ultrasomDistance() {
+  long duration;
+  float distance_cm = 0.0;
 
+  for (int i = 0; i < NUM_READINGS; ++i) {
     // Gere um pulso no pino do trigger
     digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
+    delayMicroseconds(10);
     digitalWrite(triggerPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(triggerPin, LOW);
@@ -65,24 +64,62 @@ int ultrasomDistance(){
     // Meça o tempo que o sinal leva para retornar
     duration = pulseIn(echoPin, HIGH);
 
-    // Calcule a distância em centímetros e polegadas
-    distance_cm = duration / 29.1 / 2.0;
+    // Certifique-se de que calibracao não seja zero
+    if (calibracao != 0) {
+      // Calcule a distância em centímetros e some aos resultados anteriores
+      distance_cm += duration / calibracao;
+    }
 
-    return distance_cm;
+    // Aguarde um curto intervalo antes da próxima leitura
+    delay(100);
+  }
+
+  // Calcule a média das distâncias medidas
+  distance_cm /= NUM_READINGS;
+
+  return distance_cm;
 }
 
-int moveServo(){
-    // Gire o servo para a posição inicial (0 graus)
-    myservo.write(0);
-    delay(1000);  // Aguarde por 1 segundo
+void moveServo(int angle) {
+  // Gire o servo para uma posição intermediária (90 graus)
+  myservo.write(angle);
+  delay(1000);
 
-    // Gire o servo para uma posição intermediária (90 graus)
-    myservo.write(90);
-    delay(1000);  // Aguarde por 1 segundo
+  if (angle == 90) {
+    Serial.println("Alerta de tanque em níveis críticos: Abrindo as comportas");
+    Serial.println();
+  }
+}
 
-    // Gire o servo para a posição final (180 graus)
-    myservo.write(180);
-    delay(1000);  // Aguarde por 1 segundo
+void calibrarSensor() {
+  Serial.println("Aguarde... Calibrando o sensor.");
 
-    return 1;
+  // Espera inicial
+  delay(3000);
+
+  float distanciaMedia = 0.0;
+
+  for (int i = 0; i < numLeiturasCalibracao; ++i) {
+    calibracao = calibracao + 0.15;  // Evitar divisão por zero
+
+    float distance = ultrasomDistance();
+
+    Serial.println("Distância: " + String(distance) + "cm");
+
+    if (distance > 9.99 && distance < 10.10) {
+      Serial.println("Calibração concluída");
+      Serial.println();
+      return;
+    }
+
+    delay(10);
+  }
+
+  // Calcule a média das leituras válidas
+  distanciaMedia /= numLeiturasCalibracao;
+
+  calibracao = distanciaMedia / 14.0;
+
+  Serial.println("Calibração concluída. Valor de calibração: " + String(calibracao));
+  Serial.println();
 }
